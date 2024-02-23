18cdeec9f1081d6a12bd4ddb64f5f2037d0ab3a1
Kseniya Vasileva <vasilieva.kss@gmail.com>
2024-02-23 21:01:59 +0400

    upd StarRating component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./componennts/StarRating";
 // import "./index.css";
 // import App from "./componennts/App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating onSetRating={setMovieRating} />
+      <div>Test {movieRating}</div>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
