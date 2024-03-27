75163a5ac8ae7f868243a5662162a34514fe588d
Kseniya Vasileva <vasilieva.kss@gmail.com>
2024-03-27 19:41:29 +0400

    close movie detail on click escape

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -78,6 +78,21 @@ export default function MovieDetail({
     onCloseMovie();
   }
 
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") onCloseMovie();
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
